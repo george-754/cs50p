c031a35eedce29ce3db0fb47a229ec87a572a2a0
george-754 <georgehopkins25@gmail.com>
2022-08-12 22:49:55 -0400

    finished project0 tip

---
+++ tip/tip.py
@@ -0,0 +1,26 @@
+# Tip calculator
+
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    
+    x = d[1:]
+
+    return float(x)
+
+
+def percent_to_float(p):
+    
+    z = p[:-1]
+
+    z = float(z) / 100
+    
+    return float(z)
+
+
+main()
